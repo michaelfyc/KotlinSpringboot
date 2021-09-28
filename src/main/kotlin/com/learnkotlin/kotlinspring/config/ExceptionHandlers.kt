18ad36510e140837bc3fd3a747c70b6dd50285b3
michaelfyc <michael.fyc@hdu.edu.cn>
2021-09-29 01:35:21 +0800

    feat: printStackTrace when encounter Exception

--- src/main/kotlin/com/learnkotlin/kotlinspring/config/ExceptionHandlers.kt
+++ src/main/kotlin/com/learnkotlin/kotlinspring/config/ExceptionHandlers.kt
@@ -14,6 +14,7 @@ class ExceptionHandlers {
      */
     @ExceptionHandler(value = [MethodArgumentNotValidException::class])
     fun invalidMethodParameterExceptionHandler(e: MethodArgumentNotValidException): ResultVO<Any> {
+        e.printStackTrace()
         val fieldErrors = e.bindingResult.fieldErrors
         val errorMessages = fieldErrors.map { it.defaultMessage }.toList().joinToString(";")
         return ResultVO(BadRequestException(message = errorMessages))
@@ -21,11 +22,13 @@ class ExceptionHandlers {
 
     @ExceptionHandler(value = [IllegalArgumentException::class])
     fun illegalArgumentExceptionHandler(e: IllegalArgumentException): ResultVO<Any> {
+        e.printStackTrace()
         return ResultVO(BadRequestException(message = e.message!!))
     }
 
     @ExceptionHandler(value = [CommonExceptions::class])
     fun commonExceptionHandler(e: CommonExceptions): ResultVO<Any> {
+        e.printStackTrace()
         return ResultVO(e)
     }
 }
