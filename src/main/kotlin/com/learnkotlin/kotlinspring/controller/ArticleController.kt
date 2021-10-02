6e9cc712e9749c3090d6e50009a4e2290fe5117d
michael <michael.fyc@hdu.edu.cn>
2021-10-02 16:46:52 +0800

    feat: edit article-related controller to support access by role

--- src/main/kotlin/com/learnkotlin/kotlinspring/controller/ArticleController.kt
+++ src/main/kotlin/com/learnkotlin/kotlinspring/controller/ArticleController.kt
@@ -1,6 +1,7 @@
 package com.learnkotlin.kotlinspring.controller
 
 import com.learnkotlin.kotlinspring.entity.Article
+import com.learnkotlin.kotlinspring.enums.CommonRoles
 import com.learnkotlin.kotlinspring.exceptions.BadRequestException
 import com.learnkotlin.kotlinspring.service.impl.ArticleServiceImpl
 import com.learnkotlin.kotlinspring.util.ResultVO
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
@@ -31,11 +33,11 @@ class ArticleController {
         article.authorId = uid
         logger.info("<ArticleController.createArticle>article:$article")
         val articleId = articleServiceImpl.createArticle(article)
-        return ResultVO(StatusOK("create post successfully"), articleId)
+        return ResultVO(StatusOK("create post successfully"), mapOf("article_id" to articleId))
     }
 
-    @GetMapping("/articles")
-    fun listArticleByAuthorId(authorId: Int): ResultVO {
+    @GetMapping("/user/articles")
+    fun listArticleByAuthorId(@RequestParam("uid") authorId: Int): ResultVO {
         require(authorId > 0) {
             throw BadRequestException()
         }
@@ -44,24 +46,36 @@ class ArticleController {
         return ResultVO(StatusOK(), articles)
     }
 
+    @GetMapping("/articles")
+    fun listArticles(@FromToken("role") role: CommonRoles?): ResultVO {
+        val userRole = role ?: CommonRoles.GUEST
+        val articles = articleServiceImpl.listArticles(userRole)
+        return ResultVO(StatusOK(), articles)
+    }
+
     @GetMapping("/article")
-    fun getArticleByArticleId(articleId: Int): ResultVO {
+    fun getArticleByArticleId(articleId: Int, @FromToken("role") role: CommonRoles?): ResultVO {
         require(articleId > 0) {
             throw BadRequestException()
         }
+        // if visitor hasn't signed in, there will be no token.
+        // therefore, assign a guest role for them
+        val userRole = role ?: CommonRoles.GUEST
         logger.info("<ArticleController.getArticleByArticleId>articleId:$articleId")
         val article =
-            articleServiceImpl.getArticleByArticleId(articleId) ?: return ResultVO(StatusNotFound("article not found"))
+            articleServiceImpl.getArticleByArticleId(articleId, userRole)
+                ?: return ResultVO(StatusNotFound("article not found"))
         return ResultVO(StatusOK(), article)
     }
 
     @PostMapping("/hide")
-    fun setVisibilityByArticleId(@RequestBody isVisible: Boolean, articleId: Int): ResultVO {
+    @NeedRole(CommonRoles.ADMIN)
+    fun setArticleAccessibleRole(@RequestBody role: CommonRoles, articleId: Int): ResultVO {
         require(articleId > 0) {
             throw BadRequestException()
         }
         logger.info("<ArticleController.setVisibilityByArticleId>articleId:$articleId")
-        articleServiceImpl.setVisibilityByArticleId(articleId, isVisible)
-        return ResultVO(StatusOK("successfully set article $articleId visibility to $isVisible"))
+        articleServiceImpl.setArticleAccessibleRole(articleId, role)
+        return ResultVO(StatusOK("successfully set article $articleId accessible to $role"))
     }
 }
