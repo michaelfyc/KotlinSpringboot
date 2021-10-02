291f96f9f5b302eddb4debc4271b678f8ed06e16
michael <michael.fyc@hdu.edu.cn>
2021-10-02 15:31:37 +0800

    feat: reimplement article-related service methods with role and add ListArticles method

--- src/main/kotlin/com/learnkotlin/kotlinspring/service/IArticleService.kt
+++ src/main/kotlin/com/learnkotlin/kotlinspring/service/IArticleService.kt
@@ -1,12 +1,13 @@
 package com.learnkotlin.kotlinspring.service
 
 import com.learnkotlin.kotlinspring.entity.Article
+import com.learnkotlin.kotlinspring.enums.CommonRoles
 
 interface IArticleService {
     /**
      * 显示某用户的所有帖子
      */
-    fun listArticleByAuthorId(authorId: Int): ArrayList<Article>?
+    fun listArticleByAuthorId(authorId: Int): List<Article>
 
     /**
      * 新建帖子，并返回帖子 id
@@ -16,10 +17,15 @@ interface IArticleService {
     /**
      * 由帖子 id 获取帖子
      */
-    fun getArticleByArticleId(articleId: Int): Article?
+    fun getArticleByArticleId(articleId: Int, role: CommonRoles = CommonRoles.GUEST): Article?
 
     /**
-     * 隐藏/显示帖子
+     * 设置帖子只能被特定角色的权限访问
      */
-    fun setVisibilityByArticleId(articleId: Int, isVisible: Boolean)
+    fun setArticleAccessibleRole(articleId: Int, role: CommonRoles)
+
+    /**
+     * 获取特定角色所有能看到的帖子
+     */
+    fun listArticles(role: CommonRoles): List<Article>
 }
