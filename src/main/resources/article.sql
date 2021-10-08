803412cfc96dd6e28c09861afe026cb85f5040b6
michaelfyc <michael.fyc@hdu.edu.cn>
2021-10-08 21:49:03 +0800

    fix: supplement missing `create_at` field for article.sql

--- src/main/resources/article.sql
+++ src/main/resources/article.sql
@@ -4,7 +4,8 @@ CREATE TABLE `article`
     `title`      varchar(50) not null default '',
     `content`    varchar(50) not null default '',
     `author_id`  int         not null,
+    `create_at`  datetime(6) not null default '1000-01-01 00:00:00.00000',
     /* default article is public to all roles */
-    `rid` int not null default 4,
+    `rid`        int         not null default 4,
     primary key (article_id)
 );
